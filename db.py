acc1287ef3ba26e104128da95276445e409faa88
noemabbbg <77731720+noemabbbg@users.noreply.github.com>
2022-03-09 19:31:23 +0300

    Update db.py

--- db.py
+++ db.py
@@ -5,28 +5,25 @@ import array
 
 
 
+basa='testdatabase1.db'
 
 
 class Database():
     def __init__(self, db_file):
-        self.connection=sqlite3.connect(db_file)
+        self.connection=sqlite3.connect(basa)
         self.cursor=self.connection.cursor()
 
 
     def add_user(self,user_id):
         with self.connection:
             return self.cursor.execute("INSERT INTO 'users' ('user_id') VALUES (?)", (user_id,))
+            self.connection.commit()
     def user_exists(self, user_id):
         with self.connection:
             result=self.cursor.execute("SELECT * FROM 'users' WHERE user_id = (?)", (user_id,)).fetchall()
-            return bool(len(result))!=0
-    def user_clicks (self, user_id):
-        with self.connection:
-            result=self.cursor.execute("SELECT * FROM 'users' WHERE user_id= (?)", (user_id,)).fetchall()
-            return int(result[0][1]) 
-    def set_clicks(self, user_id, clicks):
-        with self.connection:
-            return self.cursor.execute("UPDATE 'users' SET 'clicks' = ? WHERE user_id=?",(clicks, user_id,))
+            self.connection.commit()
+            return bool(len(result))
+            
     def user_money(self, user_id):
         with self.connection:
             result=self.cursor.execute("SELECT * FROM 'users' WHERE user_id = (?)", (user_id,)).fetchall()
@@ -57,44 +54,111 @@ class Database():
     def pay_subcribe(self, user_id, newmoney):
         with self.connection:
             return self.cursor.execute("UPDATE 'users' SET 'money' = ? WHERE user_id=?",(newmoney, user_id,))
+    def state_broadcast_boxer(self, user_id):
+        with self.connection:
+            result=self.cursor.execute("SELECT * FROM 'users' WHERE user_id=?",(user_id,)).fetchall()
+            return result[0][4]
+    def add_user_broadcast_boxer(self,user_id, boxerbroadcast):
+        with self.connection:
+            return self.cursor.execute("UPDATE 'users' SET 'boxerbroadcast' = ? WHERE user_id=?",(boxerbroadcast, user_id,))
+    def state_broadcast_suicideboy(self, user_id):
+        with self.connection:
+            result=self.cursor.execute("SELECT * FROM 'users' WHERE user_id=?",(user_id,)).fetchall()
+            return result[0][5]
+    def add_user_broadcast_suicideboy(self,user_id, suicideBoy):
+        with self.connection:
+            return self.cursor.execute("UPDATE 'users' SET 'suicideboybroadcast' = ? WHERE user_id=?",(suicideBoy, user_id,))
+    def state_broadcast_bastard(self, user_id):
+        with self.connection:
+            result=self.cursor.execute("SELECT * FROM 'users' WHERE user_id=?",(user_id,)).fetchall()
+            return result[0][6]
+    def add_user_broadcast_bastard(self,user_id, bastard):
+        with self.connection:
+            return self.cursor.execute("UPDATE 'users' SET 'bastardbroadcast' = ? WHERE user_id=?",(bastard, user_id,))
+    def state_broadcast_antifanatka(self, user_id):
+        with self.connection:
+            result=self.cursor.execute("SELECT * FROM 'users' WHERE user_id=?",(user_id,)).fetchall()
+            return result[0][7]
+    def add_user_broadcast_antikanatka(self,user_id, bastard):
+        with self.connection:
+            return self.cursor.execute("UPDATE 'users' SET 'antikanatkabroadcast' = ? WHERE user_id=?",(bastard, user_id,))
 
-        
-class get():
 
-    def get_user():
+    def state_broadcast_queenwithscalpel(self, user_id):
+        with self.connection:
+            result=self.cursor.execute("SELECT * FROM 'users' WHERE user_id=?",(user_id,)).fetchall()
+            return result[0][8]
+    def add_user_broadcast_queenwithscalpel(self,user_id, bastard):
+        with self.connection:
+            return self.cursor.execute("UPDATE 'users' SET 'queenwithscalpelbroadcast' = ? WHERE user_id=?",(bastard, user_id,))
         
-            sqlite_connection = sqlite3.connect('new.db')
+
+    def state_broadcast_odnazhprinc(self, user_id):
+        with self.connection:
+            result=self.cursor.execute("SELECT * FROM 'users' WHERE user_id=?",(user_id,)).fetchall()
+            return result[0][9]
+    def add_user_broadcast_odnazhprinc(self,user_id, bastard):
+        with self.connection:
+            return self.cursor.execute("UPDATE 'users' SET 'odnazhprincbroadcast' = ? WHERE user_id=?",(bastard, user_id,))
+
+    def state_broadcast_chertovka(self, user_id):
+        with self.connection:
+            result=self.cursor.execute("SELECT * FROM 'users' WHERE user_id=?",(user_id,)).fetchall()
+            return result[0][10]
+    def add_user_broadcast_chertovka(self,user_id, bastard):
+        with self.connection:
+            return self.cursor.execute("UPDATE 'users' SET 'chertovkabroadcast' = ? WHERE user_id=?",(bastard, user_id,))
+        ###блок BUFFER
+    def addbuffer(self,user_id, buffer):
+        with self.connection:
+            return self.cursor.execute("UPDATE 'users' SET 'buffer' = ? WHERE user_id=?",(buffer, user_id,))
+    def statebuffer(self, user_id):
+        with self.connection:
+            result=self.cursor.execute("SELECT * FROM 'users' WHERE user_id=?",(user_id,)).fetchall()
+            self.connection.commit()
+            return result[0][12]
+            ###блок Search
+    def addsearch(self,user_id, search):
+        with self.connection:
+            self.connection.commit()
+            return self.cursor.execute("UPDATE 'users' SET 'search' = ? WHERE user_id=?",(search, user_id,))
+            
+    def statesearch(self, user_id):
+        with self.connection:
+            result=self.cursor.execute("SELECT * FROM 'users' WHERE user_id=?",(user_id,)).fetchall()
+            self.connection.commit()
+            return result[0][11]
+    
+
+
+
+class get():  # сделаю буфферную k которая будет овтечать за то, какой столбец я беру - с айди или тех кто на что подписан  
+    def get_user(k):
+        
+            sqlite_connection = sqlite3.connect(basa)
             cursor = sqlite_connection.cursor()
 
             sqlite_select_query = """SELECT * from users"""
             cursor.execute(sqlite_select_query)
-            #row=cursor.fetchall()
             rows = cursor.fetchall()
             g=[0,]
             for row in rows:  
-                g.append(row[0])
+                g.append(row[k])
             return(g)
-           
-              #  for i in records:
-            #           print(row[0])
-             #           g.append(row[0])
-                   
-                        
-           #     return g
 
-    def get_clicksp():
+    def get_user_num(k):
         
-            sqlite_connection = sqlite3.connect('new.db')
+            sqlite_connection = sqlite3.connect(basa)
             cursor = sqlite_connection.cursor()
 
             sqlite_select_query = """SELECT * from users"""
             cursor.execute(sqlite_select_query)
-            #row=cursor.fetchall()
             rows = cursor.fetchall()
-            gu=[0,]
-            gc=[1,]
-            for row in rows:
-                gu.append(row[0])  
-                gc.append(row[1])
-            return(gu,gc)
+            g=0
+            for row in rows:  
+                g+=1
+            return(g)
     
+    
+
+
